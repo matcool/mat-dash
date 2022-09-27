26591cdf7a2fce5977e11bd295f68f15233aa19d
matcool <26722564+matcool@users.noreply.github.com>
2022-09-27 18:46:52 -0300

    add matdash::create_console() to easily create a console

---
+++ include/matdash/console.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <consoleapi.h>
+
+namespace matdash {
+	struct Console {
+		std::ofstream out, in;
+		Console() {
+			AllocConsole();
+			out = decltype(out)("CONOUT$", std::ios::out);
+			in = decltype(in)("CONIN$", std::ios::in);
+			std::cout.rdbuf(out.rdbuf());
+			std::cin.rdbuf(in.rdbuf());
+		}
+		~Console() {
+			out.close();
+			in.close();
+		}
+	};
+
+	inline void create_console() {
+		static Console console;
+	}
+}
