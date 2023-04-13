70ea1dc15d650133ff924cd619166af4d5bd7f16
Andrey <kezin636@gmail.com>
2023-04-13 14:32:43 +0300

    change minhook to dobby, that's will be better

--- include/matdash/minhook.hpp
+++ include/matdash/minhook.hpp
@@ -1,12 +1,9 @@
 #pragma once
-#include <MinHook.h>
+#include <dobby.h>
+
 
 inline void matdash::add_hook_impl(void* addr, void* detour, void** trampoline) {
-	static bool initialized = false;
-	if (!initialized) {
-		MH_Initialize();
-		initialized = true;
-	}
-	MH_CreateHook(addr, detour, trampoline);
-	MH_EnableHook(addr);
+
+	DobbyHook((void*)(addr), detour, trampoline);
+
 }
